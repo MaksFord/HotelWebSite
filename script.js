d5e3bc35d074778dc2d3bae6c67c301a3955dd5f
MaksFord <118970421+MaksFord@users.noreply.github.com>
2023-05-02 17:58:49 +0600

    better theme colors

--- script.js
+++ script.js
@@ -4,7 +4,7 @@ selectLanguage(language);
 const body = document.body;
 const savedTheme = localStorage.getItem("theme");
 if (savedTheme === "dark") {
-  body.classList.toggle("darkTheme");
+  body.classList.add("darkTheme");
 }
 
 const ruText = document.getElementById("ruLang");
